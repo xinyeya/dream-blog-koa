ef9555ce5b097c2904c9ca4678b04738713d12a2
心叶 <1329132389@qq.com>
2020-06-01 16:16:31 +0800

    修复数据库长连接多次连接的bug

--- modules/db.js
+++ modules/db.js
@@ -1,12 +1,28 @@
-const mysql = require('./config')
+// const config = require('./config')
+const mysql = require('mysql')
 
-let db = (sql) => {
-    return new Promise((resolve, reject) => {
-        mysql.query(sql, (error, results, fields)=>{
-            if (error) {
-                reject(error)
+var pool  = mysql.createPool({
+    host     : 'localhost', //host
+    user     : 'root', //数据库用户名
+    password : 'root', //密码
+    database : 'dream_blog' //数据库名
+});
+
+let db = function( sql ) {
+    return new Promise(( resolve, reject ) => {
+        pool.getConnection(function(err, connection) {
+            if (err) {
+                resolve( err )
+            } else {
+                connection.query(sql, ( err, rows) => {
+                    if ( err ) {
+                        reject( err )
+                    } else {
+                        resolve( rows )
+                    }
+                    connection.release()
+                })
             }
-            resolve(results)
         })
     })
 }
