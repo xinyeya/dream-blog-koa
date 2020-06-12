81b57b52f91e74faed615f30ef11d31b5874af30
心叶 <1329132389@qq.com>
2020-06-12 11:35:39 +0800

    完成修改jiekou

--- routes/admin/articles/index.js
+++ routes/admin/articles/index.js
@@ -14,33 +14,33 @@ router.post('/insert', upload.single('file'), async ctx => {
         let path = ctx.req.file.path
         g.path = path.replace(/public/,"").replace(/\\/g,"/")
         ctx.body = '封面上传完成'
-        return
+        return false
     }
 
     // 判断
     if (!body.title || body.title == '') {
         ctx.body = '请输入标题'
-        return
+        return false
     }
 
     if (!body.desc || body.desc == '') {
         ctx.body = '请输入简介'
-        return
+        return false
     }
 
     if (!body.content || body.content == '') {
         ctx.body = '请输入文章内容'
-        return
+        return false
     }
 
-    if (!body.label || body.label == '' || body.label === []) {
+    if (!body.label_id || body.label_id == '' || body.label_id === []) {
         ctx.body = '请选择标签分类'
         return
     }
 
     if (g === {}) {
         ctx.body == '请上传封面图片'
-        return
+        return false
     }
 
     if (!body.status || body.status == '') {
@@ -58,10 +58,10 @@ router.post('/insert', upload.single('file'), async ctx => {
 
     // 获取当前时间
     body.create_time = new Date().toLocaleDateString()
-    body.label = body.label.toString()
+    body.label_id = body.label_id.toString()
 
     if (path_status) {
-        let sql = "INSERT INTO articles (`title`, `desc`, `content`, `images`, `create_time`, `label_id`, `status`) VALUES ('" + body.title + "', '" + body.desc + "', '" + body.content + "', '" + body.images + "', '" + body.create_time + "','" + body.label + "','" + body.status + "')";
+        let sql = "INSERT INTO articles (`title`, `desc`, `content`, `images`, `create_time`, `label_id`, `status`) VALUES ('" + body.title + "', '" + body.desc + "', '" + body.content + "', '" + body.images + "', '" + body.create_time + "','" + body.label_id + "','" + body.status + "')";
 
         let res = await db(sql)
         if (res.affectedRows === 1) {
@@ -103,12 +103,11 @@ router.get('/', async ctx => {
 
 // 修改文章状态
 router.put('/state', async ctx => {
-    console.log(ctx.request.body)
     let { id, status } = ctx.request.body
 
     if (!id || id == '') {
         ctx.body = {code: 500, msg: '请选择要修改状态的文章的id'}
-        return
+        return false
     }
 
     let sql = "UPDATE articles SET `status`=" + status + " WHERE id=" + id
@@ -120,12 +119,12 @@ router.put('/state', async ctx => {
     }
 })
 
-// 删除音乐
+// 删除文章
 router.delete('/del', async ctx => {
 
     if (!ctx.request.body.id) {
         ctx.body = {code: 500, msg: '请选择要删除的文章'}
-        return
+        return false
     }
 
     let idArr = ctx.request.body.id
@@ -137,6 +136,99 @@ router.delete('/del', async ctx => {
         ctx.body = {code: 200, msg: '删除成功'}
     }else{
         ctx.body = {code: 500, msg: '删除失败'}
+    }
+})
+
+// 查询单个音乐
+router.get('/editshow', async ctx => {
+    if (!ctx.request.query.id || ctx.request.query.id == '' || ctx.request.query.id == 'undefined') {
+        ctx.body = {code: 500, msg: '请选择要修改的文章id'}
+        return false
+    }
+
+    let id = ctx.request.query.id
+
+    let sql = "SELECT `id`,`title`,`desc`,`content`,`images`,`label_id` FROM articles WHERE id="+ id
+
+    let res = await db(sql)
+
+    if(res) {
+        ctx.body = {code: 200, msg: '成功', data: res}
+    }else{
+        ctx.body = {code: 500, msg: '失败'}
+    }
+})
+
+// 修改数据
+router.put('/update', upload.single('file'), async ctx => {
+    let path_status = false
+    let body = ctx.request.body
+
+    // 判断头像是否上传了
+    if (ctx.req.file !== undefined){
+        let path = ctx.req.file.path
+        g.path = path.replace(/public/,"").replace(/\\/g,"/")
+        ctx.body = '封面上传完成'
+        return false
+    }
+
+    // 判断
+    if (!body.id || body.id == '' || body.id == 'undefined') {
+        ctx.body = '请选择要修改的id'
+        return false
+    }
+
+    if (!body.title || body.title == '') {
+        ctx.body = '请输入标题'
+        return false
+    }
+
+    if (!body.desc || body.desc == '') {
+        ctx.body = '请输入简介'
+        return false
+    }
+
+    if (!body.content || body.content == '') {
+        ctx.body = '请输入文章内容'
+        return false
+    }
+
+    if (!body.label_id || body.label_id == '' || body.label_id === []) {
+        ctx.body = '请选择标签分类'
+        return
+    }
+
+    if (g === {}) {
+        ctx.body == '请上传封面图片'
+        return false
+    }
+
+    if (!body.status || body.status == '') {
+        body.status = 0
+    }
+
+    // 修改封面路径
+    if (body.images == ''){
+        let images_path = 'http://' + ctx.request.header.host + g.path
+        body.images = images_path
+        path_status = true
+    }else{
+        path_status = true
+    }
+
+    // 获取当前时间
+    body.create_time = new Date().toLocaleDateString()
+    body.label_id = body.label_id.toString()
+
+    if (path_status) {
+        let sql = "UPDATE articles SET `title`='" + body.title + "', `desc`='" + body.desc + "', `content`='" + body.content + "',`images`='" + body.images + "',`create_time`='" + body.create_time + "', `label_id`='" + body.label_id + "', `status`=" + body.status + " WHERE id=" + body.id
+
+        let res = await db(sql)
+        if (res.affectedRows === 1) {
+            ctx.body = {code: 200, msg: '修改成功'}
+        }else{
+            ctx.body = {code: 500, msg: '修改失败'}
+        }
     }
 })
 
